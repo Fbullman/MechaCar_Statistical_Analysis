bbf8d7b250710e1ff08bcad9862f5c20fd413580
Fbullman <88170636+Fbullman@users.noreply.github.com>
2021-12-10 13:45:26 -0800

    challenge

--- README.md
+++
@@ -1,4 +0,0 @@
-# MechaCar_Statistical_Analysis
-## Linear Regression to Predict MPG
-
-this is an image!
---
+++ car_mecha_challenge.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(dplyr)
+
+#Deliverable 1
+### Perform Multiple Regression on MechaCar dataset
+
+# Read in the csv file.
+mecha_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = F,check.names = F)
+
+# Perform a linear regression module 
+mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_data)
+
+# Determine the p-value and r-squared of the linear regression module.
+summary(mecha_lm)
+
+
+#Deliverable 2
+#read Suspension_coil.csv file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#total_summary dataframe
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+# lot_summary dataframe
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#Deliverable 3
+# t.test function
+t.test(Suspension_Coil$PSI,mu = 1500)
+# t.test No.1
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# t.test No.2
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# t.test No.3
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
